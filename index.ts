d2ea0c9cea2af423ba7fd1206feecd86d82a725f
Asiya <latifnaved621@gmail.com>
2024-04-19 12:13:30 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+import inquirer from 'inquirer'
+
+const answer :{
+    sentence: string
+}= await inquirer.prompt(
+ [
+     {
+        name : "sentence",
+        type: "input",
+        message:  "Enter your sentence for count the word:"
+
+     }
+ ]
+)
+
+const words = answer.sentence.trim().split(" ")
+
+console.log(words)
+
+console.log(`your sentence word after count is: ${words.length}`);
